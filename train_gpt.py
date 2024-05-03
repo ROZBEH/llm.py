667e70fc9be5c197d5f0d57aa450db515baa6822
rozbeh <rouzbeh.asghari@gmail.com>
2024-05-03 08:32:49 -0700

    fix attention forward

--- train_gpt.py
+++ train_gpt.py
@@ -58,14 +58,7 @@ if __name__ == "__main__":
     T = 2
     C = 4
     NH = 2
-    inp = np.array(
-        [
-            [
-                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-                [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-            ]
-        ]
-    )
+    inp = np.array([[range(1, 13), range(13, 25)]])
     out = np.zeros((B, T, C))
     preatt = np.zeros((B, NH, T, T))
     att = np.zeros((B, NH, T, T))
